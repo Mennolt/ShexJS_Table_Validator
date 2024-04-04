a8623fb5024acdadfec13f2f51e385e9c2e67324
Mennolt van Alten <mennoltvanalten01@gmail.com>
2024-04-04 12:27:55 +0200

    Added support for Node Constraint Violation Added rowcount information to tableRows for use in table printing

--- HTML_TESTING/DataTransform.js
+++ HTML_TESTING/DataTransform.js
@@ -14,16 +14,62 @@ function createArray() {
 	
 	var tableRows = goThroughJSON(x[0].appinfo);
 	
-	for (var i = 0; i < tableRows.length; i++){
-		//add the item to the front of the row
-		tableRows[i].item = x[0].node
-	}
-	
 	//sort rows, starting on the last column and going backwards towards the first
 	tableRows.sort(create_compare_by_attr('error_type'))
 	tableRows.sort(create_compare_by_attr('value'))
 	tableRows.sort(create_compare_by_attr('property'))
 	
+	//calculate the required rowcounts for each 
+	var prop_counts = [0]
+	var value_counts = [0]
+	last_value = tableRows[0].value
+	last_prop = tableRows[0].property
+	for (var i = 0 ; i < tableRows.length ; i++){
+		//Property
+		if (tableRows[i].property == last_prop) {
+			prop_counts[prop_counts.length-1] += 1
+		} else {
+			prop_counts.push(1)
+			last_prop = tableRows[i].property
+		}
+		//value
+		if (tableRows[i].value == last_value) {
+			value_counts[value_counts.length-1] += 1
+		} else {
+			value_counts.push(1)
+			last_value = tableRows[i].value
+		}
+	}
+	//for item, property, and value, specify the required rowcount. For items not to be shown, use 0
+	var value_countdown = 0
+	var prop_countdown = 0
+	for (var i = 0 ; i < tableRows.length; i++){
+		//add the item to the front of the row, with rowcount
+		var rowcount = tableRows.length * (i == 0);
+		tableRows[i].item = {text : x[0].node, rowcount : rowcount};
+		
+		//add the right rowcount for value and Property
+		if (value_countdown == 0) {
+			tableRows[i].value = {text : tableRows[i].value, rowcount : value_counts[0]};
+			value_countdown = value_counts.pop()
+		} else {
+			tableRows[i].value = {text : tableRows[i].value, rowcount : 0}
+			value_countdown -= 1
+		}
+		if (prop_countdown == 0) {
+			tableRows[i].property = {text : tableRows[i].property, rowcount : prop_counts[0]}
+			prop_countdown = prop_counts.pop()
+		} else {
+			tableRows[i].property = {text : tableRows[i].property, rowcount : 0}
+			prop_countdown -= 1
+		}
+	}
+	
+	//TODO: Get names of all items likely to be in table using SPARQL Query (get query from my python thing)
+	//TODO: Use SPARQL Query to convert links into nicer values with embedded links
+	//TODO: Create MakeCell function that adds a single cell to the specified row of a table using addElement
+	//TODO: Create MakeRow function that creates a row in the tablebody using addElement, with rowID depending on the item this validation was originally For
+	//TODO: Make a function that takes (converted and niceified) tableRows and creates the entire table
 	
 	addElement('output_container', 'div', 'test', '[Item, Property, value, shape, issue type]') ;
 	addElement('output_container', 'div', 'test', JSON.stringify(tableRows)) ;
@@ -33,27 +79,33 @@ function createArray() {
 
 function goThroughJSON(data) {
 	//takes JSON validation report
-	//returns Array of Arrays which represents a table
+	//returns Array of JSON Objects which represents a table
 	
 	var output = []
 	const objectConstructor = ({}).constructor
 	
 	if (data.errors instanceof Array) {
-		for (var i = 0 ; i < data.errors.length; i++) {
-			//recurse if there are more errors in the JSON object inside this one
-			addition = goThroughJSON(data.errors[i])
-			//this makes it so in the end regardless of the amount of recursion done the system has an array representing the table, 
-			//where each element is a dictionary representing a row
-			
-			console.log(addition)
-			if (addition.constructor === objectConstructor) { //comparisaon based on https://stackoverflow.com/questions/11182924/how-to-check-if-javascript-object-is-json
-				//if the addition is already an Array of JSON objects, add it directly
-				output = output.concat(addition)
-			} else {
-				//otherwise, wrap it first
-				output = output.concat(addition)
+		//catching NodeConstraintViolations with a single string as their list of errors
+		if (typeof(data.errors[0]) === 'string') {
+			output = [createArrayRow(data)]
+		} else {
+		
+			for (var i = 0 ; i < data.errors.length; i++) {
+				//recurse if there are more errors in the JSON object inside this one
+				addition = goThroughJSON(data.errors[i])
+				//this makes it so in the end regardless of the amount of recursion done the system has an array representing the table, 
+				//where each element is a dictionary representing a row
+				
+				console.log(addition)
+				if (addition.constructor === objectConstructor) { //comparisaon based on https://stackoverflow.com/questions/11182924/how-to-check-if-javascript-object-is-json
+					//if the addition is already an Array of JSON objects, add it directly
+					output = output.concat(addition)
+				} else {
+					//otherwise, wrap it first
+					output = output.concat(addition)
+				}
+				
 			}
-			
 		}
 	} else if (data.errors instanceof Object){
 		//recurse if errors is a single JSON dict
@@ -80,23 +132,32 @@ function goThroughJSON(data) {
 
 function createArrayRow(item) {
 	//takes an item of one of the types that requires outputting 1 row
-	//creates an array item representing a row in the output data
+	//creates a JSON object representing a row in the output data
 	//output row template: {property, value, error type}
 	
 	var output = {}
-	console.log(item)
+	//console.log(item)
 	if (item.type == "MissingProperty") {
 		output.property =  item.property;
 		output.value = null; 
 		output.error_type = item.type
-	} else {
+	} else if (item.type == "NodeConstraintViolation"){
+		output.error_type = item.type
+		output.value = item.node.value
+		output.property = null
+		output.error_fulltext = item.errors[0]
+	}else {
 		output.error_type = item.type
 		output.error_fulltext = item
+		output.property = null
+		output.value = null
 	}
 	return output
 }
 
 function create_compare_by_attr(attr){
+	//Returns a function that takes two JSON objects, and compares their 'attr' attribute
+	//This returned function can be used to sort an Array of JSON Objects on said attribute stably using ArrayName.sort(TheNewFunction)
 	return function(a,b) {
 		if (a[attr] > b[attr]){
 			return 1
